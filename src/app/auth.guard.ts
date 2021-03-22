b6f37444ccc328f6b9d5fa6a3b1762ff349d1d9b
Ericadp.developer <ericadp.developer@gmail.com>
2021-03-22 11:41:42 -0400

    Update auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,3 +1,10 @@
+/*
+Title: Nodebucket
+Author: Erica Perry
+Date:03/20/21
+Description: 
+*/
+
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
@@ -10,7 +17,8 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private cookieService: CookieService) {
 
   }
-
+  
+/* creating an CanActivate with an if, else statement */
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
 
      const sessionUser = this.cookieService.get('session_user');
