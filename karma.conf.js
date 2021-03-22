ed3dcf4b8dc2de495aaa25c58526ef69efaee1e8
Ericadp.developer <ericadp.developer@gmail.com>
2021-03-22 11:46:39 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,10 @@
+/*
+Title: Nodebucket
+Author: Erica Perry
+Date:03/20/21
+Description:
+*/
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
