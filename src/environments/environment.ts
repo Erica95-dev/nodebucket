14a0b925c53804d643bb63b60c4cd78b394cba5c
Ericadp.developer <ericadp.developer@gmail.com>
2021-03-22 11:44:40 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,10 @@
+/*
+Title: Nodebucket
+Author: Erica Perry
+Date:03/20/21
+Description: 
+*/
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
