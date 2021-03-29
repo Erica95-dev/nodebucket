946d45f9ed57af1822311c9e0119584983836a39
Ericadp.developer <ericadp.developer@gmail.com>
2021-03-28 23:11:38 -0400

    Update auth.guard.ts

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,6 +1,21 @@
+/*Title: auth
+ * Author: Erica Perry
+ * Date:3/25/21
+ * Description: auth
+ */
+
+/**
+* required imports
+*/
+
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+
+/**
+* export class
+* constructor
+*/
 
 @Injectable({
   providedIn: 'root'
